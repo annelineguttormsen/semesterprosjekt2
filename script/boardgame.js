a29d0c2a8f41a740554246b4dbdbb3607ad0450d
Anneline <anneline.guttormsen@hotmail.no>
2019-11-13 20:46:32 +0100

    oppdatering klasse og calculateposition

--- script/boardgame.js
+++ script/boardgame.js
@@ -1,63 +1,76 @@
 const canvas = document.getElementById("boardgame");
 const ctx = canvas.getContext("2d");
 
-let boardPlaceNumber = 1;
-let tokenXPos;
-let tokenYPos;
-let YPosRow;
-
 let dice;
+let newBoardPlaceNumber;
 
-let token = {
-    boardPlaceNumber: 1,
-    xPos: 0,
-    yPos: 0,
-    img: "#",
-    updatePosition: function() {
+class Player {
+    constructor(boardPlaceNumber,xPos,yPos,img) {
+        this.boardPlaceNumber = boardPlaceNumber;
+        this.xPos = xPos;
+        this.yPos = yPos;
+        this.img = img;
+    }
+    updatePosition() {
         console.log("updatePosition er tilkalt");
+    }
+    animateSliding() {
+        console.log("animateSliding er tilkalt");
     }
 }
 
-function calculatePosition(boardPlaceNumber) {
+//lag players ut av player klassen
+let player1 = new Player(1,20,20,"#");
+let player2 = new Player(1,20,20,"#");
+
+function rollDice(token) {
+    dice = Math.floor(Math.random()*6)+1;
+    token.boardPlaceNumber += dice;
+    calculatePosition(token, token.boardPlaceNumber);
+    console.log("du rullet " + dice + " og er nå på " + token.boardPlaceNumber);
+    //return dice;
+}
+
+function calculatePosition(token, boardPlaceNumber) {
     //regn ut hvor Y er ut fra 6 mulige y posisjoner
-    YPosRow = Math.trunc((boardPlaceNumber/6)-0.1);
-    tokenYPos = YPosRow*75;
+    let YPosRow = Math.trunc((boardPlaceNumber/6)-0.1);
+    token.yPos = (YPosRow*90)+20;
     //X pos varierer om den skal gå til høyre eller venstre
     //sjekk y pos for om det er heltall om den skal gå til venstre
     if (YPosRow%2==0) {
         if (boardPlaceNumber%6 == 0) {
-            tokenXPos = 520;
+            token.xPos = 470;
         } else {
-            tokenXPos = (((boardPlaceNumber%6)-1)*100+20);
+            token.xPos = (((boardPlaceNumber%6)-1)*90+20);
         }
     }
     //hvis y er et deltall skal x posisjon gå til høyre
     if (YPosRow%2==1) {
         if (boardPlaceNumber%6 == 0) {
-            tokenXPos = 20;
+            token.xPos = 20;
         } else {
-            tokenXPos = 620-((boardPlaceNumber%6)*100);
+            token.xPos = 560-((boardPlaceNumber%6)*90);
         }
     }
-    console.log("X posisjon er: " + tokenXPos + " og Y posisjon er: " + tokenYPos);
+    console.log("X posisjon er: " + token.xPos + " og Y posisjon er: " + token.yPos);
     console.log("Regnet ut row: " + (YPosRow + 1));
     update();
-}calculatePosition(boardPlaceNumber);
-function rollDice() {
-    dice = Math.floor(Math.random()*6)+1;
-    boardPlaceNumber += dice;
-    calculatePosition(boardPlaceNumber);
-    console.log("du rullet " + dice + " og er nå på " + boardPlaceNumber);
-    //return dice;
 }
+
 function update() {
     ctx.clearRect(0,0,canvas.width,canvas.height);
-    updateToken();
-}
-function updateToken() {
+    drawTokens();
+}update();
+
+function drawTokens() {
     ctx.beginPath();
     ctx.fillStyle="#000000";
-    ctx.rect(tokenXPos,tokenYPos,100,75);
+    ctx.rect(player1.xPos,player1.yPos,90,90);
+    ctx.fill();
+    ctx.closePath();
+    ctx.beginPath();
+    ctx.fillStyle="tomato";
+    ctx.rect(player2.xPos,player2.yPos,90,90);
     ctx.fill();
     ctx.closePath();
 }
