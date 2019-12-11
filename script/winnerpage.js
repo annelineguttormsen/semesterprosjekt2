0f56e0ff748bc067d4052f7a47c6cbc422db9947
Anneline <anneline.guttormsen@hotmail.no>
2019-12-11 21:57:17 +0100

    podium graphic for mobil + cheater div

--- script/winnerpage.js
+++ script/winnerpage.js
@@ -11,15 +11,21 @@ const winnerGraphicWinner = document.querySelector(".winner-graphic__winner");
 const winnerGraphicLoser = document.querySelector(".winner-graphic__loser");
 const playerText = document.querySelector(".winner-hero__text");
 
+const cheaterDiv = document.querySelector(".cheater-div");
+const winnerHero = document.querySelector(".winner-hero");
+const winnerGraphic = document.querySelector(".winner-graphic");
+
 let imgNrWinner = localStorage.getItem("winnerId");
 let winnerName = localStorage.getItem("winnerName");
 let imgNrLoser = localStorage.getItem("loserId");
 let loserName = localStorage.getItem("loserName");
 
-if (imgNrWinner !== undefined && winnerName !== undefined) {
+if (imgNrWinner !== null && winnerName !== null) {
     winnerGraphicWinner.src = characterImg[imgNrWinner];
     winnerGraphicLoser.src = characterImg[imgNrLoser];
     playerText.innerHTML = winnerName + " won!";
 } else {
-    //vis cheater div
+    cheaterDiv.style.display = "block";
+    winnerHero.style.display = "none";
+    winnerGraphic.style.display = "none";
 }
